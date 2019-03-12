c7b9560573a5dfdabc2b7b2b829e5d73636d5d44
sonypark <sonypark0204@gmail.com>
2019-03-12 19:42:59 +0900

    Create calc-gui
    - Create button layout
    - Create js file for button function

---
+++ calculator/calc-gui.js
@@ -0,0 +1,8 @@
+var clickNumbers = function (event) {
+    console.log("click numbers")
+    console.log(event.target.innerHTML)
+};
+var clickOps = function (event) {
+    console.log("click operations")
+    console.log(event.target.innerHTML)
+};
