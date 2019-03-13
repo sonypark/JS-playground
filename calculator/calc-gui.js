27fcc4b3439c7a1cf2071b3471d6046f6d88245d
sonypark <sonypark0204@gmail.com>
2019-03-13 16:55:09 +0900

    Convert switch statement to if else statement

--- calculator/calc-gui.js
+++ calculator/calc-gui.js
@@ -10,20 +10,16 @@ var clickNumbers = function (event) {
     str = event.target.innerHTML;
     console.log(str);
 
-    switch (str) {
-        case 'del':
-            input.array.pop();
-            break;
-        case '+': // 연산자는 양 옆에 공백 추가
-        case '-':
-        case '*':
-        case '/':
-            input.array.push(' ' + str + ' ');
-            break;
-        default:
-            // 숫자일 때는 공백없이 숫자만 추가
-            input.array.push(str);
+    if(str === 'del'){
+        input.array.pop();
+    } else if(str === '+' || str === '-' || str === '*' || str === '/'){
+        // 연산자일 때 양 옆에 공백 추가
+        input.array.push(' ' + str + ' ');
+    } else{
+        // 숫자일 때는 공백없이 숫자만 추가
+        input.array.push(str);
     }
+
     if(input.array.length <1){ // 아무것도 없을 때 0 으로 표시
         output.text.innerText = '0'
     } else {
