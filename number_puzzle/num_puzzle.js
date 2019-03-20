9df2bf2f27dff5c769480bea848d329d3f76aea0
sonypark <sonypark0204@gmail.com>
2019-03-20 21:30:39 +0900

    [15-puzzle] Create functions
    - check answer
    - shuffle

--- number_puzzle/num_puzzle.js
+++ number_puzzle/num_puzzle.js
@@ -1,17 +1,28 @@
-const str = document.getElementById('str').innerHTML;
+let word1 = document.getElementById('word1');
 const word2 = document.getElementById('word2');
+const check = document.getElementById('check');
+
 
 // game 객체 생성
 var game = {};
-game.word = str.split(''); // 글자 배열
+game.random_word  = function () {
+    // words = ['HELLO', 'SEOUL', 'NEW YORK', 'SYDNEY', 'LONDON', 'TOKYO', 'SINGAPORE', 'CHICAGO', 'DANANG', 'HAWAI'];
+    words = 'HELLO,SEOUL,NEWYORK,SYDNEY,LONDON,TOKYO,SINGAPORE,CHICAGO,DANANG,HAWAI'.split(',');
+    rand_idx= Math.floor(Math.random()*words.length);
+    return words[rand_idx];
+};
+let target_word = game.random_word();
+word1.innerHTML = target_word;
+game.word = target_word.split(''); // 글자 배열
 game.btns = []; // 버튼 남을 배열
 
-for (let i = 0; i < str.length; i++) {
+for (let i = 0; i < target_word.length; i++) {
     const btn = document.createElement('button');
-    btn.innerHTML = str[i];
+    btn.innerHTML = target_word[i];
     word2.appendChild(btn);
     game.btns.push(btn)
 }
+
 
 game.copyBtnText = function () {
     for(let i = 0; i < this.word.length; i++){
@@ -41,13 +52,24 @@ var shiftToLeft = function (event) {
 };
 
 var check_word = function () {
-    let s = "";
-    for(let i = 0; i<game.word.length; i++){
-        s += game.word[i]
-    }
-    if (s === str){
-        document.getElementById('check').innerHTML = '일치합니다.'
+    if (game.word.join('') === target_word){
+        check.innerHTML = '일치합니다.'
     } else {
-        document.getElementById('check').innerHTML = '일치하지 않습니다.'
+        check.innerHTML = '일치하지 않습니다.'
     }
 };
+
+game.shuffle = function () {
+    // 50% 확률로 뒤집기
+    const toggle =  Math.floor(Math.random() * 2) === 0;
+    if(toggle){
+        swap()
+    }
+
+    // 랜덤확률로 오른쪽으로 밀
+    const n = Math.floor(Math.random() * game.word.length);
+    for(let i = 0; i < n; i++){
+        shiftToRight()
+    }
+};
+game.shuffle();
