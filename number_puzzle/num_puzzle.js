96e218ac3180d5b230bc95a4690b1d97335e2366
sonypark <sonypark0204@gmail.com>
2019-03-21 16:07:42 +0900

    [15-puzzle] Refactoring
    - Code refactoring
    - Add progress
    -

--- number_puzzle/num_puzzle.js
+++ number_puzzle/num_puzzle.js
@@ -1,58 +1,67 @@
-let word1 = document.getElementById('word1');
-const word2 = document.getElementById('word2');
-const check = document.getElementById('check');
+// html elements
+let word1 = document.getElementById('word1'); // target letters
+const word2 = document.getElementById('word2'); // buttons
+const check = document.getElementById('check'); // check answer
+let progress = document.getElementById('progress'); // progress check
 
 
-// game 객체 생성
+// game objects
 var game = {};
-game.random_word  = function () {
+game.btns = []; // 버튼 담을 배열
+game.maxPlay = 3;
+game.current = 0;
+
+game.checkAnswer = function () {
+    return this.letters.join('') === this.target_word
+};
+
+game.progress = function () {
+    if(this.checkAnswer()){
+        this.current++;
+        this.removeButtons();
+        this.init();
+        var str = "";
+        for (let i = 0; i < this.current; i++){
+            str += "0";
+        }
+        progress.innerHTML = str;
+    }
+
+    if(this.current === this.maxPlay){
+        alert("Good job! Thanks for playing")
+    }
+
+};
+
+game.getRandomWord = function () {
     // words = ['HELLO', 'SEOUL', 'NEW YORK', 'SYDNEY', 'LONDON', 'TOKYO', 'SINGAPORE', 'CHICAGO', 'DANANG', 'HAWAI'];
     words = 'HELLO,SEOUL,NEWYORK,SYDNEY,LONDON,TOKYO,SINGAPORE,CHICAGO,DANANG,HAWAI'.split(',');
-    rand_idx= Math.floor(Math.random()*words.length);
-    return words[rand_idx];
+    rand_idx = Math.floor(Math.random() * words.length);
+    this.target_word = words[rand_idx];
+    word1.innerHTML = this.target_word;
+    this.letters = this.target_word.split(''); // 글자 배열
 };
-let target_word = game.random_word();
-word1.innerHTML = target_word;
-game.word = target_word.split(''); // 글자 배열
-game.btns = []; // 버튼 남을 배열
-
-for (let i = 0; i < target_word.length; i++) {
-    const btn = document.createElement('button');
-    btn.innerHTML = target_word[i];
-    word2.appendChild(btn);
-    game.btns.push(btn)
-}
 
 
-game.copyBtnText = function () {
-    for(let i = 0; i < this.word.length; i++){
-        this.btns[i].innerHTML = this.word[i]
+game.addButtons = function () {
+    for (let i = 0; i < this.target_word.length; i++) {
+        const btn = document.createElement('button');
+        btn.innerHTML = this.target_word[i];
+        word2.appendChild(btn);
+        this.btns.push(btn)
     }
 };
 
-var swap = function (event) {
-    console.log('swap');
-    game.word.reverse();
-    game.copyBtnText();
-    check_word();
+
+game.removeButtons = function(){
+    for(let i = 0; i < this.btns.length; i++){
+        word2.removeChild(this.btns[i])
+    }
+    this.btns = [];
 };
 
-var shiftToRight = function (event) {
-    const s = game.word.pop();
-    game.word.unshift(s);
-    game.copyBtnText();
-    check_word();
-};
-
-var shiftToLeft = function (event) {
-    const s = game.word.shift();
-    game.word.push(s);
-    game.copyBtnText();
-    check_word();
-};
-
-var check_word = function () {
-    if (game.word.join('') === target_word){
+game.check_letters = function () {
+    if (this.checkAnswer()) {
         check.innerHTML = '일치합니다.'
     } else {
         check.innerHTML = '일치하지 않습니다.'
@@ -61,15 +70,64 @@ var check_word = function () {
 
 game.shuffle = function () {
     // 50% 확률로 뒤집기
-    const toggle =  Math.floor(Math.random() * 2) === 0;
-    if(toggle){
+    const toggle = Math.floor(Math.random() * 2) === 0;
+    if (toggle) {
         swap()
     }
 
-    // 랜덤확률로 오른쪽으로 밀
-    const n = Math.floor(Math.random() * game.word.length);
-    for(let i = 0; i < n; i++){
+    // 랜덤확률로 오른쪽으로 밀기
+    const n = Math.floor(Math.random() * (game.target_word.length - 1));
+    for (let i = 0; i < n; i++) {
         shiftToRight()
     }
 };
-game.shuffle();
+
+game.copyBtnText = function () {
+    for (let i = 0; i < this.letters.length; i++) {
+        this.btns[i].innerHTML = this.letters[i]
+    }
+};
+
+game.swap = function () {
+    this.letters.reverse();
+    this.copyBtnText();
+    this.check_letters();
+};
+
+game.shiftToRight = function () {
+    const s = this.letters.pop();
+    this.letters.unshift(s);
+    this.copyBtnText();
+    this.check_letters();
+};
+
+game.shiftToLeft = function () {
+    const s = this.letters.shift();
+    this.letters.push(s);
+    this.copyBtnText();
+    this.check_letters();
+};
+
+// event handler for swqp button
+var swap = function () {
+    game.swap();
+    game.progress();
+};
+
+var shiftToRight = function () {
+    game.shiftToRight();
+    game.progress();
+};
+
+var shiftToLeft = function () {
+    game.shiftToLeft();
+    game.progress();
+};
+
+game.init = function () {
+    this.getRandomWord();
+    this.addButtons();
+    this.check_letters();
+    this.shuffle();
+};
+game.init();
